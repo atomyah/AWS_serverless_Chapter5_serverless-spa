412ba20af1b16a8808346301087cf4e44ccb9785
Atom Yah <atom@yah>
2018-05-21 12:59:21 +0900

    final

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+    Region: "ap-northeast-1",
+    ApiBaseUrl: "https://nsr1iadqul.execute-api.ap-northeast-1.amazonaws.com/Prod",
+    ImageBaseUrl: "http://serverless-app-photos-yah.s3-website-ap-northeast-1.amazonaws.com/",
+    UserPoolId: "",
+    UserPoolClientId: ""
+}
